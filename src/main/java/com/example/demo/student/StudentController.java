fbef6dfe65ff2fbb47c7e7831964b304d073b15b
Souliman <souleymane.sada@gmail.com>
2023-02-04 02:13:05 +0000

    Wip

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -20,6 +20,7 @@ public class StudentController {
     private final StudentService studentService;
 
     @Autowired
+
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
